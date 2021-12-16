dffa657a1d01131639c9e89ba13ace1b4a8a9339
shelbylin13 <shelbylin13@gmail.com>
2021-12-16 14:46:54 -0600

    created Schema for apointments

---
+++ models/appointments.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Appointment = newSchema(
+  {
+    customer_name: { type: String, required: true },
+    service_id: { type: String, rewquired: true },
+    stylist_name: { type: String, required: true },
+    date: { type: String, required: true },
+    time: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('appointments', Appointment);
