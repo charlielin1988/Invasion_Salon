f9a1e77c07642ca78514527b05fe01687f82d5fa
shelbylin13 <shelbylin13@gmail.com>
2021-12-17 10:54:56 -0600

    updated Stylist schema with proper syntax

--- models/stylist.js
+++ models/stylist.js
@@ -6,7 +6,8 @@ const Stylist = new Schema(
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     contact: { type: String, required: true },
-    service_type: { type: String, required: true },
+    service_type: { type: Array, required: true },
+    availability: { type: Array, required: true },
     appointments: [{ type: Schema.Types.ObjectId, ref: 'appointments' }]
   },
   { timestamps: true }
