ab8cb8c6b0c4cb747510e85055fcb89ec1535e3d
shelbylin13 <shelbylin13@gmail.com>
2021-12-19 15:50:24 -0600

    added route to grab stylists

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const controllers = require('../controllers');
 const router = Router();
 
 router.get('/', (req, res) => res.send('This is root'));
+router.get('/stylists', controllers.getAllStylists);
+router.get('stylists/:id', controllers.getStylistById);
 router.post('/appointments', controllers.createAppointment);
 
 module.exports = router;
