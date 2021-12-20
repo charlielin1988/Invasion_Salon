a5fa218db22c9189ba6fc78caaef7bc0c56a6a12
shelbylin13 <shelbylin13@gmail.com>
2021-12-19 18:13:15 -0600

    fixed error in stylist get by id route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get('/', (req, res) => res.send('This is root'));
 router.get('/stylists', controllers.getAllStylists);
-router.get('stylists/:id', controllers.getStylistById);
+router.get('/stylists/:id', controllers.getStylistById);
 router.post('/appointments', controllers.createAppointment);
 
 module.exports = router;
