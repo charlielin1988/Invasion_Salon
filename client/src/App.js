78c7687e718c236e9437daa5c081e3b4b0ac0fb6
shelbylin13 <shelbylin13@gmail.com>
2021-12-20 11:19:01 -0600

    added client folders and files, filled out app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Switch>
+          <Routes>
+            <Route path="/" component={<Home />} />
+            <Route path="/stylists/:id" component={<Stylist />} />
+          </Routes>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ main/routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const controllers = require('..');
+const router = Router();
+
+router.get('/', (req, res) => res.send('This is root'));
+router.get('/stylists', controllers.getAllStylists);
+router.get('/stylists/:id', controllers.getStylistById);
+router.post('/appointments', controllers.createAppointment);
+router.put('/appointments/:id', controllers.updateAppointment);
+router.delete('/appointments/:id', controllers.deleteAppointment);
+router.get('/appointments', controllers.getAllAppointments);
+
+module.exports = router;
