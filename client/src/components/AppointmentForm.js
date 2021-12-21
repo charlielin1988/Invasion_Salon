73fd83e29b35950ab74f340f50ea6d8eba90ef89
shelbylin13 <shelbylin13@gmail.com>
2021-12-21 09:12:18 -0600

    built appointment form

--- client/src/components/AppointmentForm.js
+++ client/src/components/AppointmentForm.js
@@ -1,2 +1,66 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+
+const AppointmentForm = (props) => {
+  const [customerName, setCustomerName] = useState('');
+  const [serviceName, setServiceName] = useState('');
+  const [month, setMonth] = useState('');
+  const [day, setDay] = useState('');
+  const [time, setTime] = useState('');
+
+  const handleSubmit = (async = () => {
+    const res = await axios.post('localhost:3001/api/appointments');
+    console.log(customerName, serviceName, date, time);
+    console.log(res);
+  });
+  return (
+    <div className="appointment-form">
+      <h1>Book An Appointment</h1>
+
+      <form onSubmit={props.onSubmit}>
+        <input
+          type="text"
+          customerName={props.customerName}
+          onChange={props.onChange}
+          name="customerName"
+          placeholder={'Guest Name'}
+        />
+
+        <input
+          type="text"
+          serviceName={props.serviceName}
+          onChange={props.onChange}
+          name="serviceName"
+          placholder={'Service Name'}
+        />
+
+        <input
+          type="text"
+          month={props.month}
+          onChange={props.onChange}
+          name="month"
+          placeholder={'Month'}
+        />
+
+        <input
+          type="integer"
+          day={props.day}
+          onChange={props.onChange}
+          name="day"
+          placeholder={'Day'}
+        />
+
+        <input
+          type="integer"
+          time={props.time}
+          onChange={props.onChange}
+          name="time"
+          placehodler={'Time'}
+        />
+
+        <button type="submit">Book Now</button>
+      </form>
+    </div>
+  );
+};
+export default AppointmentForm;
